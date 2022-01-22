ede4f014362dbe650741a2bc2d7861b135f9a85e
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-01-22 16:56:31 +0000

    reorganized folders

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Luis Rodrigues</h1>
+      <p>
+        <a href="https://withluis.com/">Old Portfolio</a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
