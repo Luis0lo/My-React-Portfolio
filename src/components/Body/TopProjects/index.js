eba6884645ac70635feb082a7a5b4d98a081da02
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-02-11 22:30:51 +0000

    mapping through projects

--- src/components/Body/TopProjects/index.js
+++ src/components/Body/TopProjects/index.js
@@ -1,35 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import css from './topProjects.module.css'
 import portfolioImg from './portfolio-img.jpg';
-import{FaGithubSquare, FaLaptop} from 'react-icons/fa'
+import{FaGithubSquare, FaLaptop} from 'react-icons/fa';
+import projects from '../../../libs/projects-data'
 
 const TopProjects = () => {
-    return (
-        <div>
-            <div className={css.topProjects}>
+  const [data, setData] = useState(projects)
+  console.log(data)
+    return (<div>
+      {data.map((project)=>{
+        return (
+          <div key={project.id} className={css.topProjects}>
           <div className={css.mainProjectHeader}>
             <span className={css.projectTitle}>
-              Portfolio <small>(prior to School of Code)</small>
+              {project.title} <small>({project.subTitle})</small>
             </span>
             <div className={css.projectIcons}>
-            <a href="https://github.com/Luis0lo"><FaGithubSquare size="3em" title=""></FaGithubSquare></a>
-            <a href="https://withluis.com/"><FaLaptop size="3em" title=""></FaLaptop></a>
+            <a href={project.githubUrl}><FaGithubSquare size="3em" title=""></FaGithubSquare></a>
+            <a href={project.websiteUrl}><FaLaptop size="3em" title=""></FaLaptop></a>
             </div>
           </div>
           <div className={css.projectInfo}>
             <div className={css.projectDescription}>
-              <p>This is the result of my self learning journey</p>
+            {project.description.map((text)=>{
+              return (<p key={text.id}>{text.paragraph}</p>)
+            })}
+              {/* <p>This is the result of my self learning journey</p>
               <p>In this portfolio, you can find several projects using a wide
                 variety of tools.
               </p>
               <p>
                 I started with HTML and end up at the point where I had to build
                 a basic DB and deploy a 100% functional app.
-              </p>
+              </p> */}
             </div>
             <img src={portfolioImg} className={css.portfolioImg} alt="portfolio" />  
           </div>
         </div>
+        )
+      })}
+        
+            
         </div>
     )
 }
---
+++ src/libs/projects-data.js
@@ -0,0 +1,43 @@
+const projects = [
+  {
+    id: 1,
+    title: 'Portfolio',
+    subTitle: 'prior to School of code',
+    githubUrl: 'https://github.com/Luis0lo',
+    websiteUrl: 'https://withluis.com/',
+    description: [
+      { id: 0, paragraph: 'This is the result of my self learning journey' },
+      {
+        id: 1,
+        paragraph: 'In this portfolio, you can find several projects using a wide variety of tools.',
+      },
+      {
+        id: 2,
+        paragraph: 'I started with HTML and end up at the point where I had to build a basic DB and deploy a 100% functional app.',
+      },
+    ],
+    image: 'url',
+    imageAlt: 'Portfolio'
+  },
+  {
+    id: 2,
+    title: 'Portfolio',
+    subTitle: 'prior to School of code',
+    githubUrl: 'https://github.com/Luis0lo',
+    websiteUrl: 'https://withluis.com/',
+    description: [
+      { id: 0, paragraph: 'This is the result of my self learning journey' },
+      {
+        id: 1,
+        paragraph: 'In this portfolio, you can find several projects using a wide variety of tools.',
+      },
+      {
+        id: 2,
+        paragraph: 'I started with HTML and end up at the point where I had to build a basic DB and deploy a 100% functional app.',
+      },
+    ],
+    image: 'url',
+    imageAlt: 'Portfolio'
+  },
+];
+ export default projects
