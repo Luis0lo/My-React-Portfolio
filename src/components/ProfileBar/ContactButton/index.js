1371f2bdc51bfe3ca577272a3b83ca78c9abc391
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-03-14 20:01:05 +0000

    contact modal

--- src/components/ProfileBar/ContactButton/index.js
+++ src/components/ProfileBar/ContactButton/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaEnvelope } from 'react-icons/fa';
 import css from './contactButton.module.css';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 const ContactButton = () => {
+  const [modalIsOpen, setModelIsOpen] = useState(false);
+
+  // return (
+  //   <button className={css.buttonStyle}>
+  //     CONTACT
+  //     <span><FaEnvelope size="1.5em" title="contact" /></span>
+  //   </button>
+  // );
+
   return (
-    <button className={css.buttonStyle}>
-      CONTACT
-      <span><FaEnvelope size="1.5em" title="contact" /></span>
-    </button>
+    <div>
+      <button className={css.buttonStyle} onClick={() => setModelIsOpen(true)}>
+        CONTACT
+        <span>
+          <FaEnvelope size="1.5em" title="contact" />
+        </span>
+      </button>
+      <Modal  isOpen={modalIsOpen} onRequestClose={() => setModelIsOpen(false)}>
+        <div className={css.modal}>
+          <h1>Contact School of Code</h1>
+          <p>Luis Rodrigues</p>
+          <div>
+          <button className={css.buttonModal} onClick={() => setModelIsOpen(false)}>Close!</button>
+        </div>
+        </div>
+        
+      </Modal>
+    </div>
   );
 };
 
